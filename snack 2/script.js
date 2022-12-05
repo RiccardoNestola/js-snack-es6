4906465a1befd75d8f16d491a1e09dd7f872e734
RiccardoNestola <riccardinux@gmail.com>
2022-12-05 12:05:28 +0100

    add cars

---
+++ snack 2/script.js
@@ -0,0 +1,64 @@
+
+    /**
+     * 
+    Crea un array composto da 10 automobili.
+    Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+
+    Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+    Infine stampa separatamente i 3 array.
+    */
+
+    
+    var cars = [
+
+        car1 = {
+          brand: 'Mazda',
+          model: 'Mazda 3',
+          fuel_type: 'Gasoline'
+        },
+        car2 = {
+            brand: 'Mazda',
+            model: 'MX-5',
+            fuel_type: 'Gasoline'
+          },
+          car3 = {
+            brand: 'Honda',
+            model: 'Civic',
+            fuel_type: 'Hybrid'
+          },
+          car4 = {
+            brand: 'BMW',
+            model: '320',
+            fuel_type: 'Diesel'
+          },
+          car5 = {
+            brand: 'Mercedes',
+            model: 'SL',
+            fuel_type: 'Gasoline'
+          },
+          car6 = {
+            brand: 'Toyota',
+            model: 'Prius',
+            fuel_type: 'Electric'
+          },
+          car7 = {
+            brand: 'Tesla',
+            model: 'Model X',
+            fuel_type: 'Electric'
+          },
+          car8 = {
+            brand: 'VW',
+            model: 'Golf',
+            fuel_type: 'Diesel'
+          },
+          car9 = {
+            brand: 'Audi',
+            model: 'A3',
+            fuel_type: 'Diesel'
+          },
+          car10 = {
+            brand: 'Subaru',
+            model: 'Impreza',
+            fuel_type: 'Gasoline'
+          },
+      ];
